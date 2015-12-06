ab6b0c23a848eca0546525b529164710a1096126
Troels Krøgh <troels.kroegh@gmail.com>
2015-12-06 02:59:18 +0100

    UPPAAL SMC intro and new graphs

--- Figures/Graphs/gnuplot/uppaal_example/plot.s
+++ Figures/Graphs/gnuplot/uppaal_example/plot.s
@@ -1,12 +1,14 @@
 set term pdfcairo size 6.00in, 4.00in
 set output 'graph2.pdf'
-set xlabel 'set x label here'
-set ylabel 'set y label here'
-set title 'leave blank if no title is needed' 
+set xlabel 'UPPAAL time'
+set ylabel '%'
+set title 'Cumulative Probability' 
 set yrange[0:] 
 set xrange[0:]
 set grid ytics lc rgb "#bbbbbb" lw 1 lt 0
 set grid xtics lc rgb "#bbbbbb" lw 1 lt 0
 set key right bottom spacing 2 box
-plot 'data.csv' using 1:2 w l t 'What is this line?'
+plot 'data.txt' using 1:2 w l t 'Q 4', \
+     'data_2.txt' using 1:2 w l t 'Q 1'
+
                               
