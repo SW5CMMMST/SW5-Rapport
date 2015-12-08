6659fa40d452fda88eb0dba9705d2f72decade37
Troels Kroegh <troels.kroegh@gmail.com>
2015-12-08 12:46:22 +0100

    Added new plot

--- Figures/Graphs/gnuplot/uppaal/plot.s
+++ Figures/Graphs/gnuplot/uppaal/plot.s
@@ -5,10 +5,12 @@ set ylabel 'Probability'
 set title ''
 set yrange[0:1]
 set ytics 0,0.1,1
-set xrange[0:]
+set xrange[0:200000]
 set format x "%.s%c"
 set grid ytics lc rgb "#88607D8B" lw 1 lt 1
 set grid xtics lc rgb "#88607D8B" lw 1 lt 1
 set key right bottom spacing 2 box
-plot 'a.csv' using 1:2 with lines t 'Query a...' lc rgb "#03A9F4" lw 2, \
-     'b.csv' using 1:2 with lines t 'Query b...' lc rgb "#673AB7" lw 2
+plot 'q2.txt' using 1:2 with lines t 'Query 2' lc rgb "#03A9F4" lw 2, \
+     'q3.txt' using 1:2 with lines t 'Query 3' lc rgb "#8BC34A" lw 2, \
+     'q4.txt' using 1:2 with lines t 'Query 4' lc rgb "#FF5722" lw 2, \
+     'q5.txt' using 1:2 with lines t 'Query 5' lc rgb "#00BCD4" lw 2
