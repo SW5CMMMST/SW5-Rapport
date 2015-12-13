2cebcc3e5786194d375bf498585bec3050957542
TrampeTramp <sfrandsen8@gmail.com>
2015-12-13 15:36:13 +0100

    graphs

--- Figures/Graphs/gnuplot/uppaal/plot.s
+++ Figures/Graphs/gnuplot/uppaal/plot.s
@@ -13,5 +13,4 @@ set key right bottom spacing 2 box
 plot 'q2.txt' using 1:2 with lines t 'Query 2' lc rgb "#03A9F4" lw 2, \
      'q3.txt' using 1:2 with lines t 'Query 3' lc rgb "#8BC34A" lw 2, \
      'q4.txt' using 1:2 with lines t 'Query 4' lc rgb "#FF5722" lw 2, \
-     'q5.txt' using 1:2 with lines t 'Query 5' lc rgb "#000000" lw 2, \
-     'q6.txt' using 1:2 with lines t 'Old Model query 4' lc rgb "#FF00FF" lw 2
+     'q5.txt' using 1:2 with lines t 'Query 5' lc rgb "#000000" lw 2, 
